8cf52627fd45c7f98987d12d28ef861976073038
like5188 <260864866@qq.com>
2022-02-24 14:41:35 +0800

    升级sdk

--- alipay/src/main/java/com/like/alipay/AliPayUtils.kt
+++ alipay/src/main/java/com/like/alipay/AliPayUtils.kt
@@ -2,27 +2,25 @@ package com.like.alipay
 
 import android.app.Activity
 import android.text.TextUtils
-import android.util.Log
 import com.alipay.sdk.app.PayTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object AliPayUtils {
-    private val TAG = AliPayUtils::class.java.simpleName
 
     /**
      * 支付
      * @param orderInfo 订单信息
      */
-    suspend fun pay(activity: Activity, orderInfo: String): Boolean {
+    suspend fun pay(activity: Activity, orderInfo: String): Boolean = withContext(Dispatchers.IO) {
         // 用户在商户 app 内部点击付款，是否需要一个 loading 做为在钱包唤起之前的过渡，这个值设置为 true，将会在调用 pay 接口的时候直接唤起一个 loading，直到唤起 H5 支付页面或者唤起外部的钱包付款页面 loading 才消失。（建议将该值设置为 true，优化点击付款到支付唤起支付页面的过渡过程。）
         val result = PayTask(activity).payV2(orderInfo, true)
         val payResult = PayResult(result)
 
         // 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
         val resultInfo = payResult.result// 同步返回需要验证的信息
-        Log.d(TAG, "resultInfo=$resultInfo")
         val resultStatus = payResult.resultStatus
-        Log.d(TAG, "resultStatus=$resultStatus")
         // 判断resultStatus 为9000则代表支付成功。该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-        return TextUtils.equals(resultStatus, "9000")
+        TextUtils.equals(resultStatus, "9000")
     }
 }
--- sample/src/main/java/com/like/alipay/sample/MainActivity.kt
+++ sample/src/main/java/com/like/alipay/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.like.alipay.sample
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.like.alipay.AliPayUtils
@@ -16,10 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     fun alipay(view: View) {
         lifecycleScope.launch {
-            AliPayUtils.pay(
+            val result = AliPayUtils.pay(
                 this@MainActivity,
-                "app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22IQJZSRC1YMQB5HU%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fdomain.merchant.com%2Fpayment_notify&sign_type=RSA2&timestamp=2016-08-25%2020%3A26%3A31&version=1.0&sign=cYmuUnKi5QdBsoZEAbMXVMmRWjsuUj%2By48A2DvWAVVBuYkiBj13CFDHu2vZQvmOfkjE0YqCUQE04kqm9Xg3tIX8tPeIGIFtsIyp%2FM45w1ZsDOiduBbduGfRo1XRsvAyVAv2hCrBLLrDI5Vi7uZZ77Lo5J0PpUUWwyQGt0M4cj8g%3D"
+                "app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.02%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22314VYGIAGG7ZOYY%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA2&timestamp=2016-08-15%2012%3A12%3A15&version=1.0&sign=MsbylYkCzlfYLy9PeRwUUIg9nZPeN9SfXPNavUCroGKR5Kqvx0nEnd3eRmKxJuthNUx4ERCXe552EV9PfwexqW%2B1wbKOdYtDIb4%2B7PL3Pc94RZL0zKaWcaY3tSL89%2FuAVUsQuFqEJdhIukuKygrXucvejOUgTCfoUdwTi7z%2BZzQ%3D"
             )
+            if (result) {
+                Toast.makeText(this@MainActivity, "支付成功", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "支付失败", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
